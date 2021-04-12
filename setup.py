82fbf98d6581fd484f5f13c98977fa3fc25a5b4c
William Taylor-Melanson <wtaylor@upei.ca>
2021-04-12 16:18:50 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,14 +15,6 @@ setup(
     packages=find_packages(where='.'),  # Required
     python_requires='>=3.6, <4',
     install_requires=['pymannkendall', 'matplotlib', 'cryptocompare'],  # Optional
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # `pip` to create the appropriate form of executable for the target
-    # platform.
-    #
-    # For example, the following would provide a command called `sample` which
-    # executes the function `main` from this package when invoked:
     entry_points={  # Optional
         'console_scripts': [
             'crypto_kendall=crypto_kendall:main',
